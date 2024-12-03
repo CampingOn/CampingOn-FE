886edaf5a5a0081cc0f00d627af478ca7be0e4dc
이찬진 <chanjin23@naver.com>
2024-12-04 01:39:28 +0900

    feat : #3 추천 캠핑장 목록 조회

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,24 +1,62 @@
-import React, { useEffect } from 'react';
-import { useApi } from '../../hooks/useApi';
-import { campService } from '../../api/services/campService';
+import React, {useEffect} from 'react';
+import {useApi} from '../../hooks/useApi';
+import {campService} from '../../api/services/campService';
 import CampingCard from '../../components/CampingCard';
 
 function Home() {
-    const { execute: getPopularCamps, loading, error, data } = useApi(campService.getPopularCamps);
+    //인기 캠핑장 목록
+    const {
+        execute: getPopularCamps,
+        loading: loadingGetPopularCamps,
+        error: errorGetPopularCamps,
+        data: popularCamps,
+    } = useApi(campService.getPopularCamps);
+
+    //추천 캠핑장 목록
+    const {
+        execute: getMatchedCamps,
+        loading: loadingMatchedCamps,
+        error: errorMatchedCamps,
+        data: matchedCamps,
+    } = useApi(campService.getMatchedCamps);
 
     useEffect(() => {
         getPopularCamps(0);
+        getMatchedCamps(0, 3);
     }, []);
 
-    if (loading) return <div>로딩 중...</div>;
-    if (error) return <div>에러 발생: {error}</div>;
-    if (!data) return <div>캠핑 데이터가 없습니다.</div>;
+    if (loadingMatchedCamps) return <div>로딩 중...</div>;
+    if (errorMatchedCamps) return <div>에러 발생: {errorGetPopularCamps}</div>;
+    if (!matchedCamps) return <div>캠핑 데이터가 없습니다.</div>;
+
+    if (loadingGetPopularCamps) return <div>로딩 중...</div>;
+    if (errorGetPopularCamps) return <div>에러 발생: {errorGetPopularCamps}</div>;
+    if (!popularCamps) return <div>캠핑 데이터가 없습니다.</div>;
 
     return (
         <div className="p-4">
-            <h1 className="text-xl font-bold mb-4">인기 야영장</h1>
+            {matchedCamps.content && matchedCamps.content.length > 0 && (
+                <>
+                    <h1 className="text-xl font-bold mb-4">{matchedCamps.content.userName}님을 위한 추천 캠핑장</h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {matchedCamps.content.map((camp) => (
+                            <CampingCard
+                                key={camp.id}
+                                thumbImage={camp.thumbImage}
+                                name={camp.name}
+                                address={camp.address}
+                                keywords={camp.keywords || []}
+                                lineIntro={camp.lineIntro}
+                                marked={camp.marked}
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
+            <br/>
+            <h1 className="text-xl font-bold mb-4">인기 캠핑장</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {data.content.map((camp) => (
+                {popularCamps.content.map((camp) => (
                     <CampingCard
                         key={camp.id}
                         thumbImage={camp.thumbImage}
