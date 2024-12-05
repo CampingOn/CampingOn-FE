7a836e6b2d21e5f1d8e2e955a114240a0f142e37
bada <silverzoo030@gmail.com>
2024-12-06 01:45:23 +0900

    feat: #69 예약 페이지 대충 완료

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,7 +1,7 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 import { DeleteAccount, Login, MyKeyword, MyPage, ProfileView, Signup, UpdateProfile } from 'pages';
 import { NotFound, Home, Keyword} from 'pages';
-import { ReservationList } from 'pages';
+import { MyReservation, Reservation } from 'pages';
 import { Search } from 'pages';
 import CampDetail from "pages/camp/CampDetail";
 // import CampList from "pages/camp/CampList";
@@ -17,13 +17,14 @@ function Router() {
             <Route path="/keyword" element={<Keyword/>}/>
             <Route path="/signup" element={<Signup/>} />
             <Route path="/login" element={<Login/>} />
-            <Route path="/my-reservation" element={<ReservationList/>} />
+            <Route path="/my-reservation" element={<MyReservation/>} />
             <Route path="/camps/:campId" element={<CampDetail />} /> {/* 상세 페이지 경로 */}
             {/*<Route path="/searchCamps" element={<SearchCampList/>} />*/}
             <Route path="/my-page" element={<MyPage />} />
             <Route path="*" element={<Navigate to="/not-found" replace/>}/>
             <Route path="/not-found" element={<NotFound/>}/>
             <Route path="/search" element={<Search/>} />
+            <Route path="/camps/:campId/sites/:siteId" element={<Reservation/>}/>
         </Routes>
     );
 }
