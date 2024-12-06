597a579cd88103f576e6e07457f44305ebca5042
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-12-06 21:14:33 +0900

    Feat: #96 나의 예약 확인 페이지 구현
    - 나의 예약 목록
    - 예약이 없을 경우 예약이 없습니다 페이지
    - 예약이 있는 경우 각각의 카드 목록으로 나타남
    - 캠핑장 이름 누르면 캠핑장 상세 페이지로 이동
    - 예약 취소 구현
    - 후기 작성 관련해서는 따로 연결 필요함
    - 예약목록 확인 페이지 수정이 안되서 일단 보류
    
    Closes #96

--- src/components/CampReservationCard.js
+++ src/components/CampReservationCard.js
@@ -1,28 +1,120 @@
-import React from "react";
-import { Card, CardContent, Typography, Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+    Card,
+    CardContent,
+    Typography,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Snackbar,
+    Alert,
+    TextField
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useApi } from "../hooks/useApi";
+import { reservationService } from "../api/services/reservationService";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import {KeyboardTab, Start} from "@mui/icons-material";
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ReceiptLongOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined";
-import {useNavigate} from "react-router-dom";
+import {FestivalOutlined} from "@mui/icons-material";
 
-const CampReservationCard = ({ data, onClick }) => {
-    const { id, campSiteId, checkIn, checkOut, guestCnt, status, totalPrice, campResponseDto, campAddrResponseDto } = data;
+
+const CampReservationCard = ({ data }) => {
+    const {
+        id,
+        checkinDate,
+        checkoutDate,
+        checkinTime,
+        checkoutTime,
+        guestCnt,
+        status,
+        totalPrice,
+        campResponseDto,
+        campAddrResponseDto,
+        campSiteResponseDto
+    } = data;
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false); // 예약 취소 모달 상태
+    const [snackbarOpen, setSnackbarOpen] = useState(false); // Snackbar 상태
+    const [snackbarMessage, setSnackbarMessage] = useState("");
+    const [snackbarSeverity, setSnackbarSeverity] = useState("success"); // "success" | "error"
+    const [cancelReason, setCancelReason] = useState(""); // 취소 사유 상태
+    const [selectedReservationId, setSelectedReservationId] = useState(null); // 선택된 예약 ID
+    const [localStatus, setLocalStatus] = useState(status);
+    const {
+        execute: cancelReservation,
+        loading: cancelLoading,
+        error: cancelError,
+    } = useApi(reservationService.cancelReservation);
 
-    // 캠핑장 이름 클릭시 상세페이지로 이동 -> 링크 구현 필요
     const handleNameClick = () => {
-        navigate(`/details/${id}`);
+        navigate(`/camps/${campResponseDto.id}`);
     };
 
-    // 상태에 따른 버튼 텍스트 및 스타일
+    const handleCancelClick = (reservationId) => {
+        setSelectedReservationId(reservationId);
+        setOpen(true); // 예약 취소 모달 열기
+    };
+
+
+    const handleConfirmCancel = async () => {
+        try {
+            if (localStatus === "예약완료" && selectedReservationId) {
+                const requestData = {
+                    id: selectedReservationId,
+                    campId: campResponseDto.id,
+                    campSiteId: campSiteResponseDto.id,
+                    status: localStatus,
+                    cancelReason,
+                };
+
+                await cancelReservation(selectedReservationId, requestData);
+
+                // 클라이언트 상태 변경
+                setLocalStatus("예약취소");
+                setOpen(false); // 모달 닫기
+                setSnackbarMessage(`예약번호 ${selectedReservationId}: 취소되었습니다.`);
+                setSnackbarSeverity("success");
+                setSnackbarOpen(true); // Snackbar 표시
+            }
+        } catch (error) {
+            console.error("예약 취소 실패:", error);
+            setSnackbarMessage("예약 취소 중 문제가 발생했습니다. 다시 시도해주세요.");
+            setSnackbarSeverity("error");
+            setSnackbarOpen(true); // Snackbar 표시
+        }
+    };
+
+    const handleButtonClick = (reservationId) => {
+        if (status === "예약완료") {
+            handleCancelClick(reservationId); // 예약 취소 모달 열기
+            console.log(reservationId);
+        } else if (status === "체크인완료") {
+            console.log("후기 작성 모달 열기"); // 후기 작성 동작
+        }
+    };
+
+
+    const handleClose = () => {
+        setOpen(false); // 모달 닫기
+    };
+
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false); // Snackbar 닫기
+    };
+
     const getButtonProps = () => {
-        switch (status) {
-            case "RESERVED":
+        switch (localStatus) {
+            case "예약완료":
                 return { text: "예약 취소", variant: "outlined", disabled: false };
-            case "CANCELED":
+            case "예약취소":
                 return { text: "예약 취소됨", variant: "outlined", disabled: true };
-            case "COMPLETED":
+            case "체크인완료":
                 return { text: "후기 작성", variant: "contained", disabled: false };
             default:
                 return { text: "상태 없음", variant: "contained", disabled: true };
@@ -63,30 +155,36 @@ const CampReservationCard = ({ data, onClick }) => {
                         sx={{marginBottom: 2, fontWeight: 'bold'}}
                         onClick={handleNameClick}
                     >
-                        {campResponseDto.name}
+                        {campResponseDto.campName} - {campSiteResponseDto.siteType}
                     </Typography>
                     <Box sx={{ display: "flex", alignItems: "center", marginBottom: 1 }}>
                         <LocationOnOutlinedIcon sx={{ fontSize: 20, marginRight: 1, color: "green" }} />
                         <Typography variant="body2">
-                            {campAddrResponseDto.city} {campAddrResponseDto.state} {campAddrResponseDto.streetAddr} {campAddrResponseDto.city.detailedAddr}
+                            {campAddrResponseDto.streetAddr} {campAddrResponseDto.city.detailedAddr}
                         </Typography>
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "center", marginBottom: 1 }}>
                         <Start sx={{ fontSize: 20, marginRight: 1, color: "#ffc107" }} />
                         <Typography variant="body2">
-                            체크인 : {checkIn.split("T")[0]} 15:00
+                            체크인 : {checkinDate} {checkinTime}
                         </Typography>
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "center", marginBottom: 1 }}>
                         <KeyboardTab sx={{ fontSize: 20, marginRight: 1, color: "#2c387e" }} />
                         <Typography variant="body2">
-                            체크아웃: {checkOut.split("T")[0]} 11:00
+                            체크아웃: {checkoutDate} {checkoutTime}
                         </Typography>
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "center", marginBottom: 1 }}>
                         <PeopleOutlinedIcon sx={{ fontSize: 20, marginRight: 1, color: "#6573c3" }} />
                         <Typography variant="body2">
                             {guestCnt}명
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: "flex", alignItems: "center", marginBottom: 1 }}>
+                        <FestivalOutlined sx={{ fontSize: 20, marginRight: 1, color: "#ff7961" }} />
+                        <Typography variant="body2">
+                            내부시설: {campSiteResponseDto.indoorFacility}
                         </Typography>
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "center", marginBottom: 1 }}>
@@ -102,13 +200,51 @@ const CampReservationCard = ({ data, onClick }) => {
                     <Button
                         variant={buttonProps.variant}
                         color='warning'
-                        onClick={onClick}
+                        onClick={() => handleButtonClick(id)}
                         disabled={buttonProps.disabled}
                     >
                         {buttonProps.text}
                     </Button>
                 </Box>
             </Box>
+            {/* 예약 취소 확인 모달 */}
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>예약 취소</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        정말로 예약을 취소하시겠습니까? 취소 사유를 입력해주세요.
+                    </DialogContentText>
+                    <TextField
+                        fullWidth
+                        multiline
+                        rows={3}
+                        variant="outlined"
+                        margin="normal"
+                        placeholder="취소 사유를 입력하세요."
+                        value={cancelReason}
+                        onChange={(e) => setCancelReason(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        취소
+                    </Button>
+                    <Button onClick={handleConfirmCancel} color="error" autoFocus disabled={cancelLoading || !cancelReason}>
+                        확인
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            {/* Snackbar */}
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={4000}
+                onClose={handleSnackbarClose}
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+            >
+                <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: "100%" }}>
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
         </Card>
     );
 };
--- src/pages/reservation/MyReservation.js
+++ src/pages/reservation/MyReservation.js
@@ -1,32 +1,114 @@
-import { useEffect } from 'react';
-import { useApi } from 'hooks/useApi';
-import { reservationService } from 'api/services/reservationService';
+import React, { useEffect, useState } from "react";
+import {useApi} from "hooks/useApi";
+import {reservationService} from "api/services/reservationService";
+import {CampReservationCard, ScrollToTopFab} from "components";
+import {Box, Typography, CircularProgress} from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+
 
 const MyReservation = () => {
-  const { execute: getReservations, loading, error, data } = useApi(reservationService.getReservations);
+    const {
+        execute: getReservations,
+        loading: loadingReservations,
+        error: errorReservations,
+        data: reservationData
+    } = useApi(reservationService.getReservations);
+    const [reservations, setReservations] = useState([]);
+    const [page, setPage] = useState(0);
+    const [hasMore, setHasMore] = useState(true);
 
-  useEffect(() => {
-    getReservations();
-  }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error occurred</div>;
-  if (!data) return <div>No data</div>;
+    useEffect(() => {
+        getReservations(0, 5);
+    }, []);
 
-  return (
-      <div>
-        {data.content?.map(reservation => (
-            <div key={reservation.id}>
-              <h3>예약 ID: {reservation.id}</h3>
-              <p>체크인: {reservation.checkinDate} {reservation.checkinTime}</p>
-              <p>체크아웃: {reservation.checkoutDate} {reservation.checkoutTime}</p>
-              <p>인원: {reservation.guestCnt}</p>
-              <p>상태: {reservation.status}</p>
-              <p>총 가격: {reservation.totalPrice}</p>
-            </div>
-        ))}
-      </div>
-  );
+    useEffect(() => {
+        if (reservations?.content) {
+            setReservations((prev) => (page === 0 ? reservationData.content : [...prev, ...reservationData.content]));
+            setHasMore(reservationData.content.length === 5); // 5개씩 로드 되었는지 확인
+        }
+    }, [reservationData]);
+
+
+    if (loadingReservations)
+        return (
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                }}
+            >
+                <CircularProgress/>
+            </Box>
+        );
+
+    if (errorReservations)
+        return (
+            <Box sx={{padding: 4}}>
+                <Typography variant="h4" gutterBottom sx={{fontWeight: 'bold', marginTop: 4}}>
+                    나의 예약 목록
+                </Typography>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100vh",
+                    }}
+                >
+                    <Typography variant="h6" color="error">
+                        에러가 발생했습니다. 다시 시도해주세요.
+                    </Typography>
+                </Box>
+            </Box>
+        );
+
+    if (!reservationData || reservationData.content.length === 0)
+        return (
+            <Box sx={{padding: 4}}>
+                <Typography variant="h4" gutterBottom sx={{fontWeight: 'bold', marginTop: 4}}>
+                    나의 예약 목록
+                </Typography>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80vh",
+                    }}
+                >
+                    <CalendarTodayIcon sx={{fontSize: 100, color: "#ccc", marginBottom: 2}}/>
+                    <Typography variant="h6" gutterBottom>
+                        예약된 캠핑장이 없습니다.
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                        마음에 드는 캠핑장에 예약을 진행해보세요!
+                    </Typography>
+                </Box>
+            </Box>
+        );
+
+    return (
+
+        <Box sx={{padding: 4}}>
+            <Typography variant="h5" gutterBottom sx={{fontWeight: 'bold', marginTop: 4}}>
+                나의 예약 목록
+            </Typography>
+            <Box sx={{display: "flex", flexDirection: "column", gap: 2}}>
+                {reservationData.content.map((reservation) => (
+                    <CampReservationCard
+                        key={reservation.id}
+                        data={reservation}
+                    />
+                ))}
+            </Box>
+            <ScrollToTopFab/>
+        </Box>
+
+    );
 };
 
 export default MyReservation;
