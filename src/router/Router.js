efb618e9ba08c8f2df38a571619fb882e314a0e9
bada <silverzoo030@gmail.com>
2024-12-06 16:02:40 +0900

    feat: 페이지 라우터, index 등록

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,8 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import { Login, MyPage, Signup } from 'pages';
+import { Login, MyPage, Signup, OAuthSuccess } from 'pages';
 import { NotFound, Home, Keyword} from 'pages';
 import { MyReservation, Reservation } from 'pages';
-import { Search } from 'pages';
-import CampDetail from "pages/camp/CampDetail";
-import OAuthSuccess from "../pages/account/OAuthSuccess";
+import { Search, CampDetail } from 'pages';
 // import CampList from "pages/camp/CampList";
 // import CreateReview from "pages/review/CreateReview";
 // import CampSiteList from 'pages/camp/CampSiteList';
@@ -26,7 +24,7 @@ function Router() {
             <Route path="/not-found" element={<NotFound/>}/>
             <Route path="/search" element={<Search/>} />
             <Route path="/camps/:campId/sites/:siteId" element={<Reservation/>}/>
-            <Route path="/oauth/success" element={<OAuthSuccess/> }/>
+            <Route path="/oauth/success" element={<OAuthSuccess />} />
         </Routes>
     );
 }
