f5ddc008f71d1d537115cc65b59f61bce6333746
이찬진 <chanjin23@naver.com>
2024-12-09 18:25:08 +0900

    feat : #100 ProtectRoute 경로 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,8 +1,9 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import { Login, MyPage, Signup, OAuthSuccess } from 'pages';
-import { NotFound, Home, Keyword} from 'pages';
-import { MyReservation, Reservation } from 'pages';
-import { Search, CampDetail, MyBookmark } from 'pages';
+import {Login, MyPage, Signup, OAuthSuccess} from 'pages';
+import {NotFound, Home, Keyword} from 'pages';
+import {MyReservation, Reservation} from 'pages';
+import {Search, CampDetail, MyBookmark} from 'pages';
+import ProtectedRoute from "./ProtectedRoute";
 // import CampList from "pages/camp/CampList";
 // import CreateReview from "pages/review/CreateReview";
 // import CampSiteList from 'pages/camp/CampSiteList';
@@ -13,18 +14,18 @@ function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/keyword" element={<Keyword/>}/>
+            <Route path="/keyword" element={<ProtectedRoute><Keyword/></ProtectedRoute>}/>
             <Route path="/signup" element={<Signup/>}/>
             <Route path="/login" element={<Login/>}/>
-            <Route path="/my-reservation" element={<MyReservation/>}/>
-            <Route path="/my-bookmark" element={<MyBookmark/>}/>
+            <Route path="/my-reservation" element={<ProtectedRoute><MyReservation/></ProtectedRoute>}/>
+            <Route path="/my-bookmark" element={<ProtectedRoute><MyBookmark/></ProtectedRoute>}/>
             <Route path="/camps/:campId" element={<CampDetail/>}/> {/* 상세 페이지 경로 */}
             {/*<Route path="/searchCamps" element={<SearchCampList/>} />*/}
-            <Route path="/my-page" element={<MyPage/>}/>
+            <Route path="/my-page" element={<ProtectedRoute><MyPage/></ProtectedRoute>}/>
             <Route path="*" element={<Navigate to="/not-found" replace/>}/>
             <Route path="/not-found" element={<NotFound/>}/>
             <Route path="/search" element={<Search/>} />
-            <Route path="/camps/:campId/sites/:siteId" element={<Reservation/>}/>
+            <Route path="/camps/:campId/sites/:siteId" element={<ProtectedRoute><Reservation/></ProtectedRoute>}/>
             <Route path="/oauth/success" element={<OAuthSuccess />} />
         </Routes>
     );
