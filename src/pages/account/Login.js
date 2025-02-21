1283c5b4658d2e6c4de25df6d56203614e2909be
이찬진 <chanjin23@naver.com>
2025-02-21 20:12:16 +0900

    fix : oauth url 경로 수정

--- src/pages/account/Login.js
+++ src/pages/account/Login.js
@@ -80,7 +80,7 @@ function Login() {
     };
 
     const handleGoogleLogin = () => {
-        window.location.href = `${process.env.REACT_APP_API_URL}/oauth2/authorization/google`;
+        window.location.href = `${process.env.REACT_APP_API_URL}oauth2/authorization/google`;
     };
 
     return (
